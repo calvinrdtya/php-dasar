8bf099131509ac1b94438730db2f18b7979b7880
calvinnable <zeelink99@gmail.com>
2022-11-01 11:10:30 +0700

    first commit

---
+++ sampleemail.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+ 
+<body>
+<table cellspacing="0" cellpadding="0" border="0">
+<tbody>
+<td valign="top" style="padding-left: 0px; padding-top: 0px; padding-bottom: 0px; padding-right: 0px;">
+<span style="text-align: left; color: #000000; font-family: 'Arial', sans-serif; font-size: 10pt; font-weight: lighter">Regards,</span><br>
+<tr>
+ 
+<tr>
+</tr>
+<td valign="top" style="padding-left: 0px; padding-top: 0px; padding-bottom: 0px; padding-right: 0px;">
+<span style="text-align: left; color: #000000; font-family: 'Arial', sans-serif; font-size: 10pt; font-weight: bold">Evon Egontara</span><br>
+<span style="text-align: left; margin-top: 0px; margin-bottom: 0px; color: #000000; font-family: 'Arial', sans-serif; font-weight: normal; font-size: 9pt;">Network Engineer <font size="1" color="#B9B9B9">|</font> Network Infrastructure</span><br>
+</td>
+</tr>
+ 
+<tr>
+<td valign="top" style="padding-left: 0px; padding-top: 0px; padding-bottom: 0px; padding-right: 0px;">
+<img src="vivabnr.png" nosend="1" border="0" width="90" height="90" alt="juniper.net" title="juniper.net"></a>
+</td>
+</tr>
+ 
+<tr>
+<td valign="top" style="padding-left: 0px; padding-top: 0px; padding-bottom: 0px; padding-right: 0px;">
+<span style="text-align: left; color: #000000; font-family: Arial; font-size: 9pt; font-style: normal; font-weight: normal;"> Menara kadin Indonesia JI.H.R. Rasuna Said Block X-5, Kay 2-3 <font size="1" color="#B9B9B9">|</font> Jakarta Selatan 13150<br> Telp : +62 21 2220 7020 ext 7095  <font size="1" color="#B9B9B9">|</font> mobile (0857) 777-13xxx<br> </span>
+</td>
+</tr>
+ 
+<tr>
+<td valign="top" style="padding-left: 0px; padding-top: 5px; padding-bottom: 0px; padding-right: 0px;">
+<span style="text-align: left; margin-top: 0px; color: #F77A1E; font-size: 8pt; font-weight: bold; font-family: 'Calibri', sans-serif;">
+<a style="text-decoration: none; color: #F77A1E" href="http://www.juniper.net/us/en/"><font color="#F77A1E">website</font></a> <font size="1" color="#F77A1E">|</font> <a style="text-decoration: none; color: #F77A1E" href="https://maps.google.com/maps?q=menara+kadin+indonesia&ie=UTF-8&ei=W0fiUrD2DoSGrAeguoHgAQ&ved=0CAcQ_AUoAQ"><font color="#F77A1E">map</font></a> <font size="1" color="#F77A1E">|</font> <a style="text-decoration: none; color: #F77A1E" href="mailto:egontara@yahoo.com"><font color="#F77A1E">email</font></a>
+</span>
+</td>
+</tr>
+ 
+</td>
+</tr>
+ 
+</tbody>
+</table>
+</body>
+ 
+<style>
+a {color: #F77A1E;}
+</style>
+ 
+</html>
+
+
+
+
+
+
+<!-- <div><img src="http://req2.vivacosmetic.com/wp-content/uploads/2022/04/vivabnr-3.png" align="left"
+            style="padding-left: 20px; padding-top: 20px; padding-bottom: 0px; padding-right: 0px;
+            width="40" height="40"></div>
+
+
+<div>
+        <h4 id=top> Jl. Panjang Jiwo No. 42. Tenggilis Mejoyo 
+        <h4 id=top> Surabaya 60299 - Indonesia                
+        <h4 id=top> Telp: (031) 8410347, 8435657 (hunting) 
+    </div> -->
